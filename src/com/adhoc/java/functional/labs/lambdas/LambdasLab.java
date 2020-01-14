d16430b56c8c4990bacf47f7d4c7ecf6103d0ee2
winshifter <34403467+winshifter@users.noreply.github.com>
2020-01-14 17:25:47 -0600

    Update LambdasLab.java

--- src/com/adhoc/java/functional/labs/lambdas/LambdasLab.java
+++ src/com/adhoc/java/functional/labs/lambdas/LambdasLab.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 
 class LambdasLab {
 	
-	List<Integer> filterOddNumbers(int[] array) {
+	/*List<Integer> filterOddNumbers(int[] array) {
 		List<Integer> result = new ArrayList<>();
 		for(int val:array) {
 			if(val%2==1) {
@@ -18,14 +18,30 @@ class LambdasLab {
 			}
 		}
 		return result;
+	}*/
+	
+	List<Integer> filterNumbers(int[] array, Filtrable f) {
+		List<Integer> result = new ArrayList<>();
+		for (int val : array) {
+			if (f.test(val)) {
+				result.add(val);
+			}
+		}
+		return result;
 	}
 	
 
-	@Test
-	void filterOddNumbersTest() {
+	//@Test
+	/*void filterOddNumbersTest() {
 		List<Integer> result = filterOddNumbers(new int[]{1,1,2,3,5,8,13,21});
 		Assertions.assertEquals(6, result.size());
-	}
+	}*/
+	
+	@Test
+	void filterOddNumbersTest() {
+		List<Integer> result = filterNumbers(new int[]{1,1,2,3,5,8,13,21}, v -> v%2==1);
+		Assertions.assertEquals(6, result.size());
+	}	
 	
 	//TODO: rewrite filterOddNumbers to apply Strategy and accept a lambda
 	//TODO: rewrite filterOddNumbersTest to use the lambda version of filterOddNumbers
@@ -36,9 +52,14 @@ class LambdasLab {
 	@Test
 	void filterEvenNumbersTest() {
 		
-		//List<Integer> result = filterNumbers(new int[]{1,1,2,3,5,8,13,21},REPLACE_WITH_LAMBDA);
+		List<Integer> result = filterNumbers(new int[]{1,1,2,3,5,8,13,21},v -> v%2==0);
 		
-		//Assertions.assertEquals(2, result.size());
+		Assertions.assertEquals(2, result.size());
 	}
 
 }
+
+interface Filtrable{
+	boolean test(int i);
+}
+
