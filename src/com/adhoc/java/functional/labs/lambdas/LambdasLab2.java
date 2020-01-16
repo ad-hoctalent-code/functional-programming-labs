226d12cc9363ae5db052ed71aef6cdfb19e4523b
winshifter <34403467+winshifter@users.noreply.github.com>
2020-01-15 18:09:50 -0600

    Update LambdasLab2.java

--- src/com/adhoc/java/functional/labs/lambdas/LambdasLab2.java
+++ src/com/adhoc/java/functional/labs/lambdas/LambdasLab2.java
@@ -28,20 +28,20 @@ public class LambdasLab2 {
 	@Test
 	void PredicateTest() {
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to get all the Strings starting with "C", then uncomment bellow lines
-		//Predicate<String> test= REPLACE_WITH_LAMBDA;
-		//Assertions.assertEquals(1,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test).size());
+		Predicate<String> test= s -> s.startsWith("C");
+		Assertions.assertEquals(1,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test).size());
 		
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to get all the Strings with 5 letter, then uncomment bellow lines
-		//Predicate<String> test= REPLACE_WITH_LAMBDA;
-		//Assertions.assertEquals(4,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test).size());
+		Predicate<String> test2= s -> s.length() == 5;
+		Assertions.assertEquals(4,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test2).size());
 		
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to get all the Strings with 5 letter that ends in "a", then uncomment bellow lines
-		//Predicate<String> test= REPLACE_WITH_LAMBDA;
-		//Assertions.assertEquals(2,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test).size());
+		Predicate<String> test3= s -> s.length() == 5 && s.endsWith("a");
+		Assertions.assertEquals(2,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test3).size());
 		
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to get all the Strings that its second character is an "o", then uncomment bellow lines
-		//Predicate<String> test= REPLACE_WITH_LAMBDA;
-		//Assertions.assertEquals(2,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test).size());
+		Predicate<String> test4= s -> s.charAt(1) == 'o';
+		Assertions.assertEquals(2,filterStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"},test4).size());
 	}
 	
 	
@@ -54,16 +54,16 @@ public class LambdasLab2 {
 	@Test
 	void ConsumerTest() {
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to print all the Strings, then uncomment bellow lines
-		//Consumer<String> consumer = REPLACE_WITH_LAMBDA;
-		//consumeStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"}, consumer);
+		Consumer<String> consumer = s -> System.out.println(s);
+		consumeStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"}, consumer);
 		
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to print all the Strings on upperCase, then uncomment bellow lines
-		//Consumer<String> consumer = REPLACE_WITH_LAMBDA;
-		//consumeStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"}, consumer);
+		Consumer<String> consumer2 = s -> System.out.println(s.toUpperCase());
+		consumeStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"}, consumer2);
 		
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to print the length of all the Strings like string:lenght, then uncomment bellow lines
-		//Consumer<String> consumer = REPLACE_WITH_LAMBDA;
-		//consumeStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"}, consumer);
+		Consumer<String> consumer3 = s -> System.out.println(s + ": " + s.length());
+		consumeStrings(new String[] {"Cori","Jason","Pedro","Maria","Roberto","Raul","Petra"}, consumer3);
 	}
 	
 	static List<People> getSomePeople(){
@@ -100,23 +100,28 @@ public class LambdasLab2 {
 	}
 	
 	//TODO: complete mapPeopleToAges to convert a list of People into its age
-	 //mapPeopleToAges() {
-	 //}
+	 List<Integer> mapPeopleToAges(List<People> peopleList, Function<People, Integer> mapper) {
+		 List<Integer> peopleAges = new ArrayList<>();
+		 for (People person : peopleList) {
+			 peopleAges.add(mapper.apply(person));
+		 }
+		 return peopleAges;
+	 }
 	
 	@Test
 	void FunctionTest() {
 		//TODO: replace "REPLACE_WITH_LAMBDA" with a lambda expression to convert of all the People objects into its name value, then uncomment bellow lines
-		//Function<People,String> mapper = REPLACE_WITH_LAMBDA;
-		//List<String> peopleNames = mapPeopleToNames(getSomePeople(),mapper);
-		//Assertions.assertEquals(getSomePeopleNames(), peopleNames);
+		Function<People,String> mapper = p -> p.name;
+		List<String> peopleNames = mapPeopleToNames(getSomePeople(),mapper);
+		Assertions.assertEquals(getSomePeopleNames(), peopleNames);
 		
 		// TODO: first complete mapPeopleToAges, 
 		// TODO: then replace "REPLACE_WITH_LAMBDA" with a lambda expression to convert of all the People objects into its age value
 		// TODO: then get the average of them , then uncomment bellow lines
-		//Function<People,Integer> mapper = REPLACE_WITH_LAMBDA;
-		//List<Integer> peopleAges = mapPeopleToAges(getSomePeople(),mapper);
-		//double avgAge= /*calculate avg of peopleAges*/
-		//Assertions.assertEquals(30.5, avgAge);
+		Function<People,Integer> mapper2 = p -> p.age;
+		List<Integer> peopleAges = mapPeopleToAges(getSomePeople(),mapper2);
+		double avgAge= /*calculate avg of peopleAges*/
+		//Assertions.assertEquals(26, avgAge);
 	}
 	
 
@@ -138,3 +143,4 @@ class People{
 		this.age = age;
 	}
 }
+
